2542d2e9be25727712ab0f830100b8e3a226cb9a
phick-ws <fpr29.10@gmail.com>
2020-07-30 22:01:43 -0500

    initial commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const serverless = require('serverless-http')
+
+const app = express()
+
+const router = express.Router()
+
+
+router.get('/', (req,res,next) => {
+    res.status(200).json({
+   message:'ok, its working'
+  })
+})
+
+app.use('/app', router)
+
+module.exports.handler = serverless(app)
